d044cfc54d2b6dc35c57476f5a7530dd2658bca8
virean196 <gama.bruno.196@gmail.com>
2025-08-16 13:54:48 +0100

    Basic edits in the messages from Explore

--- commandExplore.go
+++ commandExplore.go
@@ -10,10 +10,11 @@ func commandExplore(location []string, cfg *Config) error {
 	if len(location) < 2 {
 		return fmt.Errorf("no location selected")
 	}
+	fmt.Printf("Exporing %s...\nFound pokemon: \n", location[1])
 	urlToFetch := pokeapi.GetBaseUrl() + "location-area/" + location[1]
 	nameList, err := cfg.pokeapiClient.PokemonFromLocation(urlToFetch)
 	for i := range nameList {
-		fmt.Printf("%s\n", nameList[i])
+		fmt.Printf("- %s\n", nameList[i])
 	}
 	return err
 }
