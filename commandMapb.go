e5ca8ef591ce6e086c296c2ee402d647937c403e
virean196 <gama.bruno.196@gmail.com>
2025-08-16 11:03:47 +0100

    updating next and prev url on mapb command

--- commandMapb.go
+++ commandMapb.go
@@ -4,12 +4,16 @@ import "fmt"
 
 func commandMapb(cfg *Config) error {
 	if cfg.prevLocationsURL == nil {
-		return fmt.Errorf("no previous locations to print, use 'map'")
+		return fmt.Errorf("you're on the first page")
 	} else {
 		locations, err := cfg.pokeapiClient.ListLocations(*cfg.prevLocationsURL)
 		if err != nil {
 			return fmt.Errorf("error listing locations")
 		}
+
+		cfg.nextLocationsURL = &locations.Next
+		cfg.prevLocationsURL = &locations.Previous
+
 		for i := range locations.Results {
 			fmt.Printf("%s\n", locations.Results[i].Name)
 		}
